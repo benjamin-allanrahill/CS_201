08d80cac2ff572268b5117eaa52e90eaed808681
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2018-09-12 20:50:49 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CS_201
+This is a repo for my CS 201 class. Used for version control and collaboration. 
---
+++ hw0/Hello.java
@@ -0,0 +1,9 @@
+// Hello.java - Java application to say hello
+//Benjamin Allan-Rahill
+// CS 201 example
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HELLO MIDDLEBURY");
+    }
+}
