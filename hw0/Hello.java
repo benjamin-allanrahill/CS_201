a08c4a028aa3807db2f23ef4180227b6f768c3c4
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2018-09-13 21:15:36 -0400

    Updated Hello file with class

--- hw0/Hello.java
+++ hw0/Hello.java
@@ -3,7 +3,12 @@
 // CS 201 example
 
 public class Hello {
-    public static void main(String[] args) {
+    public static void main(String[] args) {    //void means it doesnt return anything
+        int a = 3;
+        int b = =5;
+        double c = (a+b) / 3.0;
+        String myname = "Benjamin"
         System.out.println("HELLO MIDDLEBURY");
+        System.out.println(c)
     }
 }
